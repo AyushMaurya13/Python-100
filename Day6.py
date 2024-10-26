32062204c3a8a1f58ae5e2d068d35cf05aee8589
Ayush Kumar Maurya <ayushmaurya01cs@gmail.com>
2024-10-26 23:15:41 +0530

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,76 @@
+# Lecture 44 😁😁 ////////////
+# Defining and calling Function
+# Function make program easy and fast
+
+#def my_function(): # def meaning is define a function 
+    #print("Hello")
+    #print("Hello")
+
+#my_function()# Here i call my function
+
+
+#def my_function():
+    #Do this
+    # Then Do this
+    # Finally Do This
+
+def addTwo(z,x):
+    return z/x
+
+x=4
+z=4
+print(addTwo(z,x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
