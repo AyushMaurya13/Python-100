a710edba6cc22032a26dfa1032b8d8c050c82de4
Ayush Kumar Maurya <ayushmaurya01cs@gmail.com>
2024-10-27 17:12:26 +0530

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -2,14 +2,14 @@
 # Defining and calling Function
 # Function make program easy and fast
 
-#def my_function(): # def meaning is define a function 
+def my_function(): # def meaning is define a function 
     #print("Hello")
     #print("Hello")
 
-#my_function()# Here i call my function
+my_function()# Here i call my function
 
 
-#def my_function():
+def my_function():
     #Do this
     # Then Do this
     # Finally Do This
